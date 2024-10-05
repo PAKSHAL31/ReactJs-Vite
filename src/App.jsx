5f07a7c1b506666a83fe7273ea3ffb26e74e4c19
PAKSHAL31 <pakshal31mr@gmail.com>
2024-10-05 10:09:50 -0500

    Toggle dark light theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import ThemeBtn from './components/ThemeBtn.jsx'
+import { ThemeProvider } from './context/theme.js'
+import { useEffect } from 'react'
+import Card from './components/card.jsx'
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', "light")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
