7c37363fc51351397fd3e6964280e9edda532771
PAKSHAL31 <pakshal31mr@gmail.com>
2024-10-05 18:16:50 -0500

    Passwrod Manager

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
-    </>
+      <Navbar/>
+      <Manager/>
+      </>
   )
 }
 
